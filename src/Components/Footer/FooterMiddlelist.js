3fc250784e5cf361b7c36907d8d8513634a4568d
Mohamed Ibrahim Teba <mohamed.i.teba@gmail.com>
2025-07-30 00:43:27 +0300

    Update FooterMiddlelist.js

--- src/Components/Footer/FooterMiddlelist.js
+++ src/Components/Footer/FooterMiddlelist.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const FooterMiddlelist = ({title,listItem}) => {
     return (
-        
             <div>
                 <h3 className='text-white text-base font-semibold pb-2'>{title}</h3>
                 <ul className='flex flex-col gap-2'>
@@ -11,9 +10,10 @@ const FooterMiddlelist = ({title,listItem}) => {
                    )) )}
                 </ul>
             </div>
-           
-        
     )
 }
 
 export default FooterMiddlelist
+
+
+
